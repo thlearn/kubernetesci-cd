7c240559d4163cb857b03e048953a5908467c517
root <root@impetus-i0315.impetus.co.in>
2019-05-15 04:25:20 -0400

    demo

--- src/main/java/com/mkyong/rest/HelloWorldService.java
+++ src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -10,7 +10,7 @@ public class HelloWorldService {
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
   
-     String output = "Jersey say : " + msg;
+     String output = "reply from rest applicaion : " + msg;
  
 		return Response.status(200).entity(output).build();
  
